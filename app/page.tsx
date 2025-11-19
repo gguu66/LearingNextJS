ac3caba1eb7ab97204fe57f0de733dcbde5b4328
gguu <3483499367@qq.com>
2025-11-19 20:42:03 +0800

    初始化

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Page(){
+export default function page() {
   return (
     <div>
-      DashBoard
+      Home
     </div>
   )
 }
--- app/test/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NotFound() {
-  return (
-    <div>
-      我是自定义局部notfound
-    </div>
-  )
-}
